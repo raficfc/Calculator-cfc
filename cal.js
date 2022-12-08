376467b55e14d8ee865f691c9dbe4ba64d5aa823
raficfc <zrafis04@gmail.com>
2022-12-08 22:18:20 +0530

    calculator v1

---
+++ cal.js
@@ -0,0 +1,17 @@
+function display(num) {
+
+    console.log(result);
+    result.value+=num
+    
+}
+function clearScreen(num) {
+    result.value="0"
+}
+function equel() {
+
+    result.value=eval(result.value)
+    
+}
+function delt() {
+    result.value=result.value.slice(0,-1)
+}
